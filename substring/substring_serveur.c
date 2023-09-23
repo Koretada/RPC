bfb1609be73501a786974eae5b2c29d871c5eb80
Koretada <koretadaa@gmail.com>
2023-09-23 17:36:14 +0200

    Changement de la signature côté serveur

--- substring/substring_serveur.c
+++ substring/substring_serveur.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdbool.h>
 #include <string.h>
+#include <stdbool.h>
+#include "substring.h"
 
-bool containsforbiddensubstr_1_svc(const char *str, const char *forbiddenSubstr1, const char *forbiddenSubstr2, const char *forbiddenSubstr3) {
+bool_t * containsforbiddensubstr_1_svc(char *str, char *forbiddenSubstr1, char *forbiddenSubstr2, char *forbiddenSubstr3, struct svc_req *rqstp) {
     int state1 = 0, state2 = 0, state3 = 0;
     int strLen = strlen(str);
     int subStrLen1 = strlen(forbiddenSubstr1);
@@ -18,7 +19,7 @@ bool containsforbiddensubstr_1_svc(const char *str, const char *forbiddenSubstr1
         if (c == forbiddenSubstr1[state1]) {
             state1++;
             if (state1 == subStrLen1) {
-                return true;  // La chaîne contient la sous-chaîne interdite 1
+                return (bool_t *) true;  // La chaîne contient la sous-chaîne interdite 1
             }
         } else {
             state1 = 0;  // Réinitialise l'état si le caractère ne correspond pas
@@ -27,7 +28,7 @@ bool containsforbiddensubstr_1_svc(const char *str, const char *forbiddenSubstr1
         if (c == forbiddenSubstr2[state2]) {
             state2++;
             if (state2 == subStrLen2) {
-                return true;  // La chaîne contient la sous-chaîne interdite 2
+                return (bool_t *) true;  // La chaîne contient la sous-chaîne interdite 2
             }
         } else {
             state2 = 0;  // Réinitialise l'état si le caractère ne correspond pas
@@ -36,27 +37,30 @@ bool containsforbiddensubstr_1_svc(const char *str, const char *forbiddenSubstr1
         if (c == forbiddenSubstr3[state3]) {
             state3++;
             if (state3 == subStrLen3) {
-                return true;  // La chaîne contient la sous-chaîne interdite 3
+                return (bool_t *) true;  // La chaîne contient la sous-chaîne interdite 3
             }
         } else {
             state3 = 0;  // Réinitialise l'état si le caractère ne correspond pas
         }
     }
 
-    return false;  // La chaîne est valide
+    return (bool_t *) false;  // La chaîne est valide
 }
 
-char *generaterandomstring_1_svc(int length) {
+char **generaterandomstring_1_svc(int length, struct svc_req *rqstp) {
     char *result = (char *)malloc((length + 1) * sizeof(char));
-    const char charset[] = "abcdefghijklmnopqrstuvwxyz";
-    const int charsetLength = strlen(charset);
+    char charset[] = "abcdefghijklmnopqrstuvwxyz";
+    int charsetLength = strlen(charset);
 
     for (int i = 0; i < length; i++) {
         result[i] = charset[rand() % charsetLength];
     }
 
     result[length] = '\0';
-    return result;
+
+    // Allouer et renvoyer un pointeur vers le résultat
+    char **p_result = (char **)malloc(sizeof(char *));
+    *p_result = result;
+
+    return p_result;
 }
-
-
