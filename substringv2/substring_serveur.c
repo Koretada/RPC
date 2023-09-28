c9dfe5753c4567dbccbc16a9b8156920f683b91e
Walid <walid.bounhach@grenoble-inp.org>
2023-09-28 16:52:47 +0200

    Test mineur

--- substringv2/substring_serveur.c
+++ substringv2/substring_serveur.c
@@ -1,3 +1,4 @@
+#include "substring.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,69 +6,73 @@
 
 #define ALPHABET_SIZE 26
 
-// Fonction pour vérifier si une sous-chaîne donnée est interdite
-int * is_forbidden_substring_1_svc(char* temp, char forbidden[3][4], struct svc_req *rqstp) {
-    // Parcourir toutes les sous-chaînes interdites
+int *
+is_forbidden_substring_1_svc(string *temp, struct svc_req *rqstp) {
+    static int result;
+    char forbidden[3][4] = {"abc", "def", "ghi"};
+    
     for (int j = 0; j < 3; j++) {
-        // Si la sous-chaîne est trouvée, retourner 1
-        if (strstr(temp, forbidden[j]) != NULL) {
-            return (int *) 1;  
+        if (strstr(*temp, forbidden[j]) != NULL) {
+            result = 1;
+            return &result;
         }
     }
-    // Si aucune sous-chaîne interdite n'est trouvée, retourner 0
-    return (int *) 0;  
+    result = 0;
+    return &result;
 }
 
-// Fonction pour générer le prochain caractère de la chaîne finale
-char * generate_next_char_1_svc(char* result, int i, char forbidden[3][4], struct svc_req *rqstp) {
-    // Définir l'alphabet complet
+string *
+generate_next_char_1_svc(string *result_str, int *pos, StringArray *forbidden, struct svc_req *rqstp) {
+    static char result_char[2];
     char alphabet[ALPHABET_SIZE] = "abcdefghijklmnopqrstuvwxyz";
-    char temp[4];  // Chaîne temporaire pour stocker les caractères en cours de traitement
-    int next_char_index;  // Index du prochain caractère à ajouter
-    int end;  // Limite pour construire la chaîne temporaire
+    char temp[4];
+    int next_char_index;
+    int end;
 
-    // Déterminer la limite en fonction de la position actuelle dans la chaîne finale
+    int i = *pos;
+
     if (i < 3) {
         end = i;
     } else {
         end = 2;
     }
 
-    // Boucle pour générer un caractère qui ne crée pas de sous-chaîne interdite
     while (1) {
-        // Générer un index aléatoire pour le prochain caractère
         next_char_index = rand() % ALPHABET_SIZE;
 
-        // Construire la chaîne temporaire pour vérification
         for (int j = 0; j <= end; j++) {
             if (j < end) {
-                temp[j] = result[i - (end - j)];
+                temp[j] = (*result_str)[i - (end - j)];
             } else {
                 temp[j] = alphabet[next_char_index];
             }
         }
-        temp[end + 1] = '\0';  // Assurer que la chaîne est bien terminée
+        temp[end + 1] = '\0';
 
-        // Si la chaîne temporaire n'est pas interdite, sortir de la boucle
-        if (!is_forbidden_substring_1_svc(temp, forbidden, rqstp)) {
-            char *ptr = (char*) malloc(sizeof(char));
-            *ptr = alphabet[next_char_index];
-            return ptr;
+        int forbidden_check = *is_forbidden_substring_1_svc(&temp, rqstp);
+
+        if (!forbidden_check) {
+            result_char[0] = alphabet[next_char_index];
+            result_char[1] = '\0';
+            return result_char;
         }
     }
 }
 
-// Fonction pour générer la chaîne finale sans sous-chaînes interdites
-void * generate_string_without_substrings_1_svc(int length, char forbidden[3][4], char* result, struct svc_req *rqstp) {
-    // Initialiser le générateur de nombres aléatoires
+string *
+generate_string_without_substrings_1_svc(int *length, StringArray *forbidden, struct svc_req *rqstp) {
+    static char result[1024];
+
     srand(time(NULL));
 
-    // Générer chaque caractère de la chaîne finale
-    for (int i = 0; i < length; i++) {
-        result[i] = *(generate_next_char_1_svc(result, i, forbidden, rqstp));
+    for (int i = 0; i < *length; i++) {
+        char *next_char = generate_next_char_1_svc(&result, &i, forbidden, rqstp);
+        result[i] = next_char[0];
     }
-    result[length] = '\0';  // Assurer que la chaîne est bien terminée
+    result[*length] = '\0';
+    return &result;
 }
+
 
 // int main() {
 //     // Définir les sous-chaînes interdites
